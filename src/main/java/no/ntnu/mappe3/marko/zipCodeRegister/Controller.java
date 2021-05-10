bd67f03c814227074205cbbd0da67294f12e3a1c
Marko19907 <marko.stancic23@gmail.com>
2021-05-10 13:50:38 +0200

    Added a table to the GUI

--- src/main/java/no/ntnu/mappe3/marko/zipCodeRegister/Controller.java
+++ src/main/java/no/ntnu/mappe3/marko/zipCodeRegister/Controller.java
@@ -1,5 +1,64 @@
 package no.ntnu.mappe3.marko.zipCodeRegister;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
+/**
+ * Class Controller represents the main controller of the application.
+ * It is responsible for handling the events from the GUI.
+ *
+ * @author Marko
+ * @version 10-05-2021
+ */
 public class Controller
 {
+    /**
+     * The PostalCodeRegister for adding, storing and reading PostalCodes
+     */
+    private final PostalCodeRegister postalCodeRegister;
+
+    /**
+     * ObservableList of the PostalCodes
+     */
+    private final ObservableList<PostalCode> postalCodeObservableList;
+
+    /**
+     * Instantiates the controller.
+     */
+    public Controller()
+    {
+        this.postalCodeRegister = new PostalCodeRegister();
+        this.postalCodeObservableList = FXCollections.observableArrayList(this.postalCodeRegister.getPostalCodes());
+
+        this.fillWithDemoEntities();
+    }
+
+    /**
+     * Fills the register with demo data
+     */
+    private void fillWithDemoEntities()
+    {
+        this.postalCodeRegister.addPostalCode(new PostalCode("TestName1", "1234"));
+        this.postalCodeRegister.addPostalCode(new PostalCode("TestName2", "4321"));
+        this.postalCodeRegister.addPostalCode(new PostalCode("TestName3", "6789"));
+
+        this.updateObservableList();
+    }
+
+    /**
+     * Updates the observable list of PostalCodes with fresh values from the register
+     */
+    private void updateObservableList()
+    {
+        this.postalCodeObservableList.setAll(this.postalCodeRegister.getPostalCodes());
+    }
+
+    /**
+     * Returns an ObservableList of PostalCodes to show
+     * @return An ObservableList of PostalCodes to show
+     */
+    public ObservableList<PostalCode> getPostalCodeObservableList()
+    {
+        return this.postalCodeObservableList;
+    }
 }
--- src/main/java/no/ntnu/mappe3/marko/zipCodeRegister/PostalCodeRegisterGUI.java
+++ src/main/java/no/ntnu/mappe3/marko/zipCodeRegister/PostalCodeRegisterGUI.java
@@ -1,11 +1,44 @@
 package no.ntnu.mappe3.marko.zipCodeRegister;
 
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
+import java.util.Arrays;
+
+/**
+ * Class PostalCodeRegisterGUI represents the main window of the application.
+ *
+ * @author Marko
+ * @version 10-05-2021
+ */
 public class PostalCodeRegisterGUI extends Application
 {
+    /**
+     * The main controller
+     */
+    private final Controller controller;
 
+    /**
+     * PostalCodeRegisterGUI constructor
+     */
+    public PostalCodeRegisterGUI()
+    {
+        this.controller = new Controller();
+    }
+
+    /**
+     * The main method
+     * @param args The command-line arguments
+     */
     public static void main(String[] args)
     {
         launch(args);
@@ -14,6 +47,53 @@ public class PostalCodeRegisterGUI extends Application
     @Override
     public void start(Stage primaryStage)
     {
+        BorderPane root = new BorderPane();
+        root.setCenter(this.setupCenter());
 
+        primaryStage.setTitle("Postal Code Register");
+        primaryStage.setMinWidth(500);
+        primaryStage.setMinHeight(600);
+
+        Scene scene = new Scene(root, 500, 600, Color.WHITE);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
+        root.requestFocus();
+    }
+
+    /**
+     * Returns the center HBox
+     * @return The center HBox
+     */
+    private HBox setupCenter()
+    {
+        HBox centerBox = new HBox();
+        centerBox.getChildren().add(this.setupCenterTable());
+        HBox.setHgrow(centerBox.getChildren().get(0), Priority.ALWAYS);
+        return centerBox;
+    }
+
+    /**
+     * Returns up the table that contains the PostalCodes
+     * @return An already set-up table that contains the PostalCodes
+     */
+    private TableView<PostalCode> setupCenterTable()
+    {
+        TableView<PostalCode> postalCodeTableView = new TableView<>();
+        postalCodeTableView.setPlaceholder(new Label("No zip codes to display"));
+        postalCodeTableView.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY);
+
+        TableColumn<PostalCode, String> zipColumn = new TableColumn<>("Zip");
+        zipColumn.setCellValueFactory(new PropertyValueFactory<>("zipCode"));
+
+        TableColumn<PostalCode, String> municipalityColumn = new TableColumn<>("Municipality");
+        municipalityColumn.setCellValueFactory(new PropertyValueFactory<>("municipalityName"));
+
+        postalCodeTableView.getColumns().addAll(Arrays.asList(zipColumn, municipalityColumn));
+        postalCodeTableView.setItems(this.controller.getPostalCodeObservableList());
+        //Set a default sort column
+        postalCodeTableView.getSortOrder().add(zipColumn);
+
+        return postalCodeTableView;
     }
 }
