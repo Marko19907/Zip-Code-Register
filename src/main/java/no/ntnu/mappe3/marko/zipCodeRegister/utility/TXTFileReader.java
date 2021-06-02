5ae9788deced5c3e77d37089c6eecbab33a60793
Marko19907 <marko.stancic23@gmail.com>
2021-06-02 11:57:08 +0200

    Improved the TXTFileReader class

--- src/main/java/no/ntnu/mappe3/marko/zipCodeRegister/utility/TXTFileReader.java
+++ src/main/java/no/ntnu/mappe3/marko/zipCodeRegister/utility/TXTFileReader.java
@@ -5,6 +5,7 @@ import no.ntnu.mappe3.marko.zipCodeRegister.model.PostalCode;
 import java.io.File;
 import java.io.IOException;
 import java.net.URISyntaxException;
+import java.net.URL;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
@@ -18,7 +19,7 @@ import java.util.logging.Logger;
  * Class TXTFileReader is responsible for reading data from text files.
  *
  * @author Marko
- * @version 11-05-2021
+ * @version 02-06-2021
  */
 public class TXTFileReader
 {
@@ -31,7 +32,7 @@ public class TXTFileReader
      * The name of the file to read the PostalCodes from
      * Must include the file name and the file extension (e.g., 'someFile.txt')
      */
-    private static final String DATA_FILE_NAME = "zipCodeData.txt";
+    private static final String DATA_FILE_NAME = "/zipCodeData.txt";
 
     /**
      * The logger
@@ -55,11 +56,9 @@ public class TXTFileReader
         List<PostalCode> postalCodeList = null;
 
         try {
-            File fileToRead = new File(
-                    Objects.requireNonNull(
-                            this.getClass().getClassLoader().getResource(DATA_FILE_NAME)).toURI()
-            );
-            postalCodeList = this.readPostalCodesList(fileToRead);
+            URL url = this.getClass().getResource(DATA_FILE_NAME);
+            File file = new File(Objects.requireNonNull(url).toURI());
+            postalCodeList = this.readPostalCodesList(file);
         }
         catch (IOException | NullPointerException | URISyntaxException e) {
             this.logger.log(Level.INFO, "Failed to read data from the txt file . . .");
@@ -71,17 +70,16 @@ public class TXTFileReader
 
     /**
      * Returns a List of PostalCodes from a given txt file.
-     * @param fileToRead The file to read from, not null
+     * @param fileToRead The File to read from, not null
      * @return A List of PostalCodes read from the given txt file
-     * @throws IllegalArgumentException If the given file is invalid
-     * @throws IOException              If an IO error is encountered
+     * @throws IOException If an IO error is encountered
      */
     private List<PostalCode> readPostalCodesList(File fileToRead) throws IOException
     {
         List<PostalCode> postalCodesList = new ArrayList<>();
 
         if (fileToRead != null && fileToRead.canRead()) {
-            List<String> lineStringList = Files.readAllLines(Paths.get(fileToRead.getAbsolutePath()), StandardCharsets.UTF_8);
+            List<String> lineStringList = Files.readAllLines(Paths.get(fileToRead.toURI()), StandardCharsets.UTF_8);
             lineStringList.forEach(row -> {
                 String[] data = row.split(SEPARATOR);
                 String zipCode = data[0];
